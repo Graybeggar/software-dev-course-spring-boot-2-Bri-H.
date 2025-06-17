09c6706155769467021e071e71296887faea7a03
Graybeggar <blhat@hotmail.com>
2025-06-17 13:22:04 -0600

    Update AlbumController.java

--- src/main/java/com/example/mycollections/controllers/AlbumController.java
+++ src/main/java/com/example/mycollections/controllers/AlbumController.java
@@ -1,4 +1,42 @@
 package com.example.mycollections.controllers;
 
+import com.example.mycollections.models.Album;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
 public class AlbumController {
+
+    private final List<Album> albums = new ArrayList<>() {{
+        add(new Album("Abbey Road", "The Beatles", 1969, 17));
+        add(new Album("Thriller", "Michael Jackson", 1982, 9));
+        add(new Album("Rumours", "Fleetwood Mac", 1977, 11));
+    }};
+
+    @GetMapping("/json")
+    public List<Album> getAlbumsJson() {
+        return albums;
+    }
+
+    @GetMapping("/html")
+    public String getAlbumsHtml() {
+        StringBuilder albumList = new StringBuilder();
+        for (Album album : albums) {
+            albumList.append("<li>").append(album).append("</li>");
+        }
+
+        return """
+                <html>
+                    <body>
+                        <h1>Albums</h1>
+                        <ul>
+                """ + albumList + """
+                        </ul>
+                    </body>
+                </html>
+                """;
+    }
 }
